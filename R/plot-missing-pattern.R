951bafe4addec641985fad696d7a60cab3f9629a
KunHuo <610112077@qq.com>
2022-08-13 11:01:28 +0800

    updated

--- R/plot-missing-pattern.R
+++ R/plot-missing-pattern.R
@@ -1,10 +1,13 @@
 #' Plot missing pattern with ggplot2
 #'
 #' @param data a data frame.
+#' @param percent a logical, show percentage or frequency.
 #' @param show.all a logical, indicate whether to show all variables, by default,
 #' only show missing variables.
 #' @param decreasing a logical. Should the sort order be increasing or decreasing?
 #' if is NULL, not sorted.
+#' @param add.var.miss  a logical, indicate whether to show missing count of each variable.
+#' @param digits digits for missing percent, defualt 2.
 #' @param language language, typically “en”, or "zh", default "en".
 #' @param font.family font family.
 #' @param font.size font size.
@@ -24,8 +27,11 @@
 #' # Specify the color
 #' gg_missing_pattern(lung, show.all = TRUE, color = c("#E85827", "#81D8D0"))
 gg_missing_pattern <- function(data,
+                               percent = FALSE,
                                show.all = FALSE,
                                decreasing = TRUE,
+                               add.var.miss = TRUE,
+                               digits = 1,
                                language  = c("en", "zh"),
                                font.family = "serif",
                                font.size = 12,
@@ -48,12 +54,29 @@ gg_missing_pattern <- function(data,
   }
 
   plotdata <- plotdata[, -ncol(plotdata), drop = FALSE]
-  plotdata <- plotdata[-nrow(plotdata), , drop = FALSE]
 
-  pattern.count <- plotdata[[1]]
+  miss.count.variable <- unlist(plotdata[nrow(plotdata), ])[-1]
+
+  plotdata <- plotdata[-nrow(plotdata), , drop = FALSE]
+  y.label <- plotdata[[1]]
+
+  if(percent){
+    y.label <- sprintf("%s%%", format_digits(as.numeric(y.label) / nrow(data) * 100, digits))
+    miss.count.variable <- sprintf("%s%%", format_digits(as.numeric(miss.count.variable) / nrow(data) * 100, digits))
+
+    y.title <- ifelse(language == "en", "Percent of missing pattern", "\u7f3a\u5931\u6a21\u5f0f\u767e\u5206\u6bd4")
+  }else{
+    y.title <- ifelse(language == "en", "Number of missing pattern", "\u7f3a\u5931\u6a21\u5f0f\u4f8b\u6570")
+  }
 
   plotdata <- plotdata[, -1, drop = FALSE]
   varnames <- names(plotdata)
+
+  if(add.var.miss){
+    x.label <- sprintf("%s (%s)", varnames, miss.count.variable)
+  }else{
+    x.label <- varnames
+  }
 
   plotdata <- reshape_long(plotdata, add.id.col = TRUE)
 
@@ -72,10 +95,11 @@ gg_missing_pattern <- function(data,
                    plot.margin = ggplot2::unit(c(0.6, 0.4, 0.4, 0.4), "cm"), ...) +
     gg_rotate_x_text() +
     gg_delete_x_title() +
-    gg_delete_y_title() +
+    gg_ylab(y.title) +
     gg_delete_legend_title() +
     ggplot2::coord_cartesian(expand = FALSE) +
-    ggplot2::scale_y_discrete(label = pattern.count) +
+    ggplot2::scale_y_discrete(label = y.label) +
+    ggplot2::scale_x_discrete(label = x.label) +
     gg_legend_position("top")
 
   if(!is.null(color)){
