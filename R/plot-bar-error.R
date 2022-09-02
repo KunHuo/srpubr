08901dcf9b77d76d3c83ef238944d95e4bdb0e65
KunHuo <610112077@qq.com>
2022-09-02 11:04:49 +0800

    Update plot-bar-error.R

--- R/plot-bar-error.R
+++ R/plot-bar-error.R
@@ -7,7 +7,26 @@ gg_bar_error <- function(data,
                          line.size = 0.25,
                          error.color = bar.color,
                          error.size = 0.25,
-                         error.width = 0.2){
+                         error.width = 0.2,
+                         language = NULL,
+                         font.family = NULL,
+                         font.size = NULL,
+                         ...
+                         ){
+
+
+  language    <- get_global_languange(language, default = "en")
+  font.family <- get_global_family(font.family, default = "serif")
+  font.size   <- get_global_fontsize(font.size, default = 12)
+  bar.color   <- get_global_palette(bar.color)
+  error.color <- get_global_palette(error.color)
+
+  if(language == "zh"){
+    sysfonts::font_add("simsun", "simsun.ttc")
+    font.family <- "simsun"
+  }else{
+    font.family <- font.family
+  }
 
   x  <- select_variable(data, x)
   y  <- select_variable(data, y)
@@ -22,20 +41,48 @@ gg_bar_error <- function(data,
   })
 
 
-  by <- ifelse(is.null(by), x, by)
+  by.legend <- ifelse(is.null(by), x, by)
 
 
   pos <- ggplot2::position_dodge(width = bar.width)
 
-  p <- ggplot2::ggplot(data = plotdata, ggplot2::aes_string(x = x, y = "value", fill = by, color = by)) +
-    ggplot2::geom_errorbar(ggplot2::aes_string(ymin = "lower", ymax = "upper"), width = error.size, position =  pos, size = error.size) +
-    ggplot2::geom_bar(stat = "identity", width = bar.width, position = pos, size = line.size) +
-    gg_theme_sci(legend.key.size = 0.8, axis.line.size = line.size) +
-    ggplot2::coord_cartesian(clip = "off")
+  p <- ggplot2::ggplot(data = plotdata, ggplot2::aes_string(x = x, y = "value", fill = by.legend, color = by.legend)) +
+    ggplot2::geom_errorbar(ggplot2::aes_string(ymin = "lower", ymax = "upper"),
+                           width = error.width,
+                           position =  pos,
+                           size = error.size) +
+    ggplot2::geom_bar(stat = "identity",
+                      width = bar.width,
+                      position = pos,
+                      size = line.size) +
+    gg_theme_sci(legend.key.size = 0.8,
+                 axis.line.size = line.size,
+                 font.family = font.family,
+                 font.size = font.size, ...) +
+    ggplot2::coord_cartesian(clip = "off") +
+    gg_ylab(y) +
+    gg_delete_x_title()
 
   ybresks <- pretty(c(0, max(plotdata[["upper"]])), n = 5)
 
-  p +
+  p <- p +
     ggplot2::scale_y_continuous(breaks = ybresks, limits = c(min(ybresks), max(ybresks)), expand = c(0, 0))
+
+
+  if(is.null(by)){
+    p <- p + gg_delete_legend()
+  }
+
+  if(!is.null(bar.color)){
+    p <- p +
+      ggplot2::scale_fill_manual(values = bar.color)
+  }
+
+  if(!is.null(error.color)){
+    p <- p +
+      ggplot2::scale_color_manual(values = error.color)
+  }
+
+  p
 }
 
